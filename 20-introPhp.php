dc9d707588a1247632aca1e4cff7cf4c61802923
74079 <63118958+74079@users.noreply.github.com>
2022-04-26 20:25:39 -0300

    Introdução - php Nivel Basico

---
+++ 20-introPhp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$txt = "PHP";
+$int = 12345;
+echo "$int - My first $txt  script!  Meu nome e Lucas";
+
+//echo saida de informacoes do tipo print 
+
+echo "<br>"; //echo quebra de linha 
+
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+
+
+?>
+
+</body>
+</html>
